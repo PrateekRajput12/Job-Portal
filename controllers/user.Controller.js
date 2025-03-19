9baeb82aca402b643a3c57d222f689b384e1f08d
PrateekRajput12 <ficredinn@gmail.com>
2025-03-19 12:52:04 +0530

    Login Logout controller created

--- controllers/user.Controller.js
+++ controllers/user.Controller.js
@@ -1,5 +1,5 @@
 import { User } from "../Models/user.Model"
-
+import jwt from 'jsonwebtoken'
 
 
 export const register = async (req, res) => {
@@ -29,7 +29,83 @@ export const register = async (req, res) => {
             password: hashedPassword,
             role
         })
+
+
+        return res.status(201).json({
+            message: "Account created successfully",
+            success: true
+        })
     } catch (error) {
         console.log("Error in registering", error);
+    }
+}
+
+
+export const login = async (req, res) => {
+    try {
+        const { email, password, role } = req.body
+        if (!email || !password || !role) {
+            return res.status(400).json({ message: "Something is missing", success: false })
+        }
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({ message: "Incorrect email or password", success: false })
+
+        }
+
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordMatch) {
+            return res.status(400).json({ message: "Incorrect email or password", success: false })
+
+        }
+
+        // Check role is correct or not
+
+        if (role !== user.role) {
+            res.status(400).json({
+                message: "Account doesn't macthed with current role",
+                success: false
+            })
+        }
+
+        const tokeData = {
+            userId: user._id
+        }
+
+        const token = await jwt.sign(tokeData, process.env.SECRET_KEY, {
+            expiresIn: '1d'
+        })
+
+        user = {
+            _id: user._id,
+            fullName: user.fullname,
+            email: user.email,
+            phoneNumber: user.phoneNumber,
+            role: user.role,
+            profile: user.profile
+        }
+
+        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
+            message: `Welcome back ${user.fullName}`,
+            user,
+            success: true
+        })
+
+    } catch (error) {
+        console.log("error in logining", error);
+    }
+}
+
+
+export const logout = async (req, res) => {
+    try {
+        return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+            message: "Logout successfully ",
+            success: true
+        })
+    } catch (error) {
+        console.log("Error in logining out", error);
     }
 }
