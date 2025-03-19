d97fcbd3c13bbbadcfa4dd951c5367ae8d45c5d0
PrateekRajput12 <ficredinn@gmail.com>
2025-03-19 12:05:21 +0530

    User Schema Created

--- Models/user.Model.js
+++ Models/user.Model.js
@@ -1,106 +1,47 @@
+import mongoose from "mongoose";
 
-// const validator = require('validator')
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["student", "recruiter"],
+        required: true
+    },
+    profile: {
+        bio: { type: String },
+        skills: [{ type: String }],
+        resume: { type: String },
+        resumeOriginalName: { type: String },
+        company: {
+            type: mongoose.Schema.Types.ObjectId,
+            red: "Company"
+        },
+        profilePhoto: {
+            type: String,
+            default: ""
+        }
+    }
 
-// const bcrypt = require('bcrypt')
-
-// const jwt = require('jsonwebtoken')
-// const mongoose = require('mongoose')
-
-
-// const userSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true
-//     },
-//     password: {
-//         type: String,
-//         required: true
-//     },
-//     profile: {
-//         title: String,
-//         summary: String,
-//         skills: [String],
-//         experiece: [
-//             {
-//                 title: String,
-//                 company: String,
-//                 location: String,
-//                 startDate: Date,
-//                 endDate: Date,
-//                 description: String,
-//                 highlights: [String]
-//             }
-//         ],
-//         education: [
-//             {
-//                 degree: String,
-//                 institution: String,
-//                 location: String,
-//                 graduationDate: Date,
-//                 description: String
-//             }
-//         ],
-
-//         certifications: [String],
-//         preferredLocations: [String],
-//         preferredJobTypes: [String]
-//     },
-//     documents: [{
-//         name: String,
-//         type: String, // 'resume', 'cover_letter', etc.
-//         content: String,
-//         file: {
-//             data: Buffer,
-//             contentType: String
-//         },
-//         createdAt: {
-//             type: Date,
-//             default: Date.now
-//         }
-//     }],
-//     preferences: {
-//         jobKeywords: [String],
-//         companySizes: [String],
-//         industries: [String],
-//         excludedCompanies: [String],
-//         salaryRange: {
-//             min: Number,
-//             max: Number
-//         },
-//         applicationVolume: {
-//             type: String,
-//             enum: ['low', 'medium', 'high'],
-//             default: 'medium'
-//         }
-//     }, createdAt: {
-//         type: Date,
-//         default: Date.now
-//     }
-
-// })
+}, { timestamps: true })
 
 
-// userSchema.methods.getJWT = async function () {
-//     const user = this
-//     const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' })
-//     return token
-// }
+export const User = mongoose.model('User', userSchema)
 
 
-
-// userSchema.methods.validatePassword = async (passwordInputByUser) => {
-//     const user = this
-//     const passwordHash = user.password
-//     const isPasswordValid = await bcrypt.compare(passwordInputByUser, passwordHash)
-
-//     return isPasswordValid
-
-// }
-
-
-// module.exports = mongoose.model("User", userSchema)
