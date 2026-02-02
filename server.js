bb377fcb9a93f2a3db5236ac38619e4c3c4e4125
PrateekRajput12 <ficredinn@gmail.com>
2026-02-02 12:42:04 +0530

    addedfrontend url

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 const corsOption = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173',
+        "https://jobportalui-black.vercel.app"
+    ],
     credentials: true
 }
 app.use(cors(corsOption))
